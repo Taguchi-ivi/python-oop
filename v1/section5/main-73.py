764fe6ae6418243e9154603dfc5cc02c4cc9b80a
taguchi kazuki <elfubolfelicidadsuprema02@gmail.com>
2025-01-05 23:47:02 +0900

    refactor: 73

--- v1/section5/main-73.py
+++ v1/section5/main-73.py
@@ -3,8 +3,10 @@
 class BankAccount:
   def __init__(self) -> None:
     self._balance = 0
+
   def deposit(self, amount: int) -> None:
     self._balance += amount
+
   def withdraw(self, amount: int) -> None:
     self._balance -= amount
 
